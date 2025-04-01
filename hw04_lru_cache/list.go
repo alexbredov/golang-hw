1ed556ea796cf61961155950791ea2ab5e3975f5
alexbredov <sanya2008rus@gmail.com>
2025-04-02 00:23:40 +0300

    HW04: List completed

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,5 +1,7 @@
 package hw04lrucache
 
+import "sync"
+
 type List interface {
 	Len() int
 	Front() *ListItem
@@ -17,10 +19,105 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	sync.RWMutex
+	length int
+	front  *ListItem
+	back   *ListItem
 }
 
+func (l *list) Len() int {
+	return l.length
+}
+
+func (l *list) Front() *ListItem {
+	return l.front
+}
+
+func (l *list) Back() *ListItem {
+	return l.back
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	if l == nil {
+		panic("list is nil")
+	}
+	l.Lock()
+	defer l.Unlock()
+	litem := &ListItem{Value: v, Prev: nil, Next: l.front}
+	if l.front != nil {
+		l.front.Prev = litem
+	}
+	if l.length == 0 {
+		l.back = litem
+	}
+	l.length++
+	return l.front
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	if l == nil {
+		panic("list is nil")
+	}
+	l.Lock()
+	defer l.Unlock()
+	litem := &ListItem{Value: v, Prev: l.back, Next: nil}
+	if l.back != nil {
+		l.back.Next = litem
+	}
+	if l.length == 0 {
+		l.front = litem
+	}
+	l.length++
+	l.back = litem
+	return l.back
+}
+
+func (l *list) Remove(item *ListItem) {
+	if l == nil {
+		panic("list is nil")
+	}
+	l.Lock()
+	defer l.Unlock()
+	if item != nil && l.length > 0 {
+		switch {
+		case l.length == 1:
+			l.front = nil
+			l.back = nil
+		case item == l.front:
+			item.Next.Prev = nil
+			l.front = item.Next
+		case item == l.back:
+			item.Prev.Next = nil
+			l.back = item.Prev
+		default:
+			item.Prev.Next = item.Next
+			item.Next.Prev = item.Prev
+		}
+		l.length--
+		item = nil
+	}
+}
+
+func (l *list) MoveToFront(item *ListItem) {
+	if l == nil {
+		panic("list is nil")
+	}
+	l.Lock()
+	defer l.Unlock()
+	if item != nil && l.length > 1 && item != l.front {
+		if item == l.back {
+			l.back = item.Prev
+			item.Prev.Next = nil
+		} else {
+			item.Prev.Next = item.Next
+			item.Next.Prev = item.Prev
+		}
+		l.front.Prev = item
+		item.Prev = nil
+		item.Next = l.front
+		l.front = item
+	}
+}
 func NewList() List {
 	return new(list)
 }
