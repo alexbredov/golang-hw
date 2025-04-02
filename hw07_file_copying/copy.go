2b71c913b1fff814d42dee965141bc0636da403a
alexbredov <sanya2008rus@gmail.com>
2025-04-02 21:36:25 +0300

    HW07: Copying files, basic tests, depguard magic

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,78 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrNegativeOffset        = errors.New("negative offset")
+	ErrNegativeLimit         = errors.New("negative limit")
+	ErrFileOpen              = errors.New("file open failed")
+	ErrFileCreate            = errors.New("file creation failed")
+	ErrNoOriginalFileSize    = errors.New("original file size undefined")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	if filepath.Ext(fromPath) == "" || filepath.Ext(fromPath) != filepath.Ext(toPath) {
+		return ErrUnsupportedFile
+	}
+	if offset < 0 {
+		return ErrNegativeOffset
+	}
+	if limit < 0 {
+		return ErrNegativeLimit
+	}
+	file, err := os.Open(fromPath)
+	if err != nil {
+		return ErrFileOpen
+	}
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return ErrFileOpen
+	}
+	defer file.Close()
+	if fileInfo.Size() <= limit {
+		limit = fileInfo.Size()
+	}
+	if offset > fileInfo.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+	if fileInfo.Size() == 0 {
+		return ErrNoOriginalFileSize
+	}
+	target, err := os.Create(toPath)
+	if err != nil {
+		return ErrFileCreate
+	}
+	defer target.Close()
+	pBar := pb.Full.Start64(limit)
+	var buf []byte
+	if limit == 0 {
+		read, err := io.ReadAll(file)
+		buf = read[offset:]
+		if err != nil {
+			return err
+		}
+	} else {
+		var bufLength int64
+		if offset+limit > fileInfo.Size() {
+			bufLength = fileInfo.Size() - offset
+		} else {
+			bufLength = limit
+		}
+		buf = make([]byte, bufLength)
+		file.ReadAt(buf, offset)
+		pBar.Finish()
+	}
+	_, err = target.Write(buf)
+	if err != nil {
+		return err
+	}
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,31 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("offset exceeds file size", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "target.txt", 10000, 1)
+		require.Truef(t, errors.Is(err, ErrOffsetExceedsFileSize), "actual error %q", err)
+	})
+	t.Run("no original file", func(t *testing.T) {
+		err := Copy("testdata/no_input.txt", "target.txt", 0, 0)
+		require.Truef(t, errors.Is(err, ErrFileOpen), "actual error %q", err)
+	})
+	t.Run("creation failure", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "target.jpg", 0, 0)
+		require.Truef(t, errors.Is(err, ErrUnsupportedFile), "actual error %q", err)
+	})
+	t.Run("negative offset", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "target.txt", -1, 0)
+		require.Truef(t, errors.Is(err, ErrNegativeOffset), "actual error %q", err)
+	})
+	t.Run("negative limit", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "target.txt", 0, -1)
+		require.Truef(t, errors.Is(err, ErrNegativeLimit), "actual error %q", err)
+	})
 }
