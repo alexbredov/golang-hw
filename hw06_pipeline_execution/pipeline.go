61235bb5b9ea27e6d985bef22912a7c11b42e81f
alexbredov <sanya2008rus@gmail.com>
2025-04-02 05:47:29 +0300

    HW06: Added empty test

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,39 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	if len(stages) == 0 {
+		return in
+	}
+	for _, stage := range stages {
+		stageOut := stage(in)
+		out := make(Bi, 1)
+		go func(stageOut In, out Bi) {
+			defer close(out)
+			for {
+				select {
+				case <-done:
+					go func() {
+						for range stageOut {
+						}
+					}()
+					return
+				case v, ok := <-stageOut:
+					if !ok {
+						return
+					}
+					select {
+					case out <- v:
+					case <-done:
+						go func() {
+							for range stageOut {
+							}
+						}()
+						return
+					}
+				}
+			}
+		}(stageOut, out)
+		in = out
+	}
+	return in
 }
