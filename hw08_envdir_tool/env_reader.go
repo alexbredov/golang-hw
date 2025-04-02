2deec06ac9ca148b11e55e54d865dfef8b03a22d
alexbredov <sanya2008rus@gmail.com>
2025-04-03 00:05:10 +0300

    HW08: Another fix

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -53,7 +53,7 @@ func ReadDir(dir string) (Environment, error) {
 				fmt.Printf("failed to read file %v: %v\n", fileName, err)
 				continue
 			}
-			line = strings.TrimRight(line, "\t '\"")
+			line = strings.Trim(line, "\t '\"")
 			envDirMap[envName] = EnvValue{Value: line, NeedRemove: false}
 			file.Close()
 		}
